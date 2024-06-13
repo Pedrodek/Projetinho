7714c0e2bf1007494d3ad8704e09858b72b35515
Pedro <110565971+Pedrodek@users.noreply.github.com>
2024-06-13 20:03:30 -0300

    Add files via upload

--- ProjetoAndre/Pagina1/script.js
+++ ProjetoAndre/Pagina1/script.js
@@ -1,6 +1,4 @@
 
-
-
 $('.box-top-page').slick({
   prevArrow: '<button type="button" class="prevArrow">&#8592;</button>',
   nextArrow: '<button type="button" class="nextArrow">&#8594;</button>',
@@ -11,10 +9,27 @@ $('.box-top-page').slick({
   adaptiveHeight: true,
   slidesToScroll: 1,
   autoplay: false,
-  autoplaySpeed: 5000
+  autoplaySpeed: 5000,
 });
 
+const faders = document.querySelectorAll('.fade-in');
 
+const appearOptions = {
+  threshold: 0.5,
+  rootMargin: "0px 0px -50px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, appearOptions);
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
 /*$("a").hover(
     function(){
       $(this).css("background-color", "red");
